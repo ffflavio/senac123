53f972d8a5101b2d13b34af847e86abddc68f56a
aluno <aluno@LAB24543CTO.edu.ce.senac.br>
2017-07-25 10:34:02 -0300

    testa

--- src/pacclass/ContaCorrentee.java
+++ src/pacclass/ContaCorrentee.java
@@ -1,14 +1,23 @@
-
 package pacclass;
 
-
-
 public class ContaCorrentee {
-    
-  
-    
+
     String nome;
     float saldo = 0;
-    
-        
+
+    float verificarSaldo() {
+        return saldo;
+    }
+
+    void depositarValor(float valor) {
+        saldo += valor;
+    }
+
+    void sacarValor(float valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+        } else {
+            System.out.println("Não tem saldo, saldo indisponivel ");
+        }
+    }
 }
